05260fe21ad5024d9bf036bd9d48dd00ab8c4c06
tndals11 <tmdals09231@naver.com>
2024-02-01 21:35:45 +0900

    2024_02_01_springframeworkJSP
    구현완료

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -4,10 +4,11 @@ import com.codingrecipe.member.dto.MemberDTO;
 import com.codingrecipe.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 @RequestMapping("/member") // RequestMapping 공통의 주소를 묶는다
@@ -18,18 +19,93 @@ public class MemberController {
 
     @GetMapping("/save")
     public String saveForm() {
-
         return "save"; // jsp를 띄어주는
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute MemberDTO memberDto) {
-        int saveResult = memberService.save(memberDto);
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        int saveResult = memberService.save(memberDTO);
 
         if (saveResult > 0) {
             return "login"; // 가입 성공
         } else {
             return "save"; // 가입 실패
         }
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginForm(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+      boolean result = memberService.login(memberDTO);
+
+      if (result) {
+          session.setAttribute("loginEmail", memberDTO.getMemberEmail());
+          return "main";
+      } else {
+          return "login";
+      }
+
+    }
+    @GetMapping("/main")
+    public String main() {
+
+        return "main";
+        }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
+    }
+
+    @GetMapping
+    public String findById(@RequestParam("id") Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+
+        model.addAttribute("member", memberDTO);
+
+        return "detail";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") Long id) {
+
+        memberService.delete(id);
+        return "redirect:/member/";
+    }
+
+    @GetMapping("/update")
+    public String update(Model model, HttpSession session) {
+        String loginEmail = (String) session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.findByMemberEmail(loginEmail);
+        model.addAttribute("member", memberDTO);
+
+        return "update";
+    }
+
+    @PostMapping("/update")
+    public String updateForm(@ModelAttribute MemberDTO memberDTO) {
+        boolean result = memberService.update(memberDTO);
+
+        if (result) {
+            return "redirect:/member?id=" + memberDTO.getId();
+        } else {
+            return "index";
+        }
+    }
+
+    @PostMapping("/email-check")
+    @ResponseBody
+    public String emailCheck(@RequestParam("memberEmail") String memberEmail) {
+        System.out.println(memberEmail);
+        String checkResult = memberService.checkEmail(memberEmail);
+
+        return checkResult;
     }
 }
--- src/main/java/com/codingrecipe/member/repository/MemberRepository.java
+++ src/main/java/com/codingrecipe/member/repository/MemberRepository.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
@@ -15,5 +17,31 @@ public class MemberRepository {
         // Member => namespace="Member"이 이름으로 들어가고
         // .save Mapper안에 id="save"의 값으로 들어간다
         return sql.insert("Member.save", memberDTO);
+    }
+
+    public MemberDTO login(MemberDTO memberDTO) {
+        // 조회 결과가 1개이므로 selectOne을 사용한다.
+        return sql.selectOne("Member.login", memberDTO);
+    }
+
+    public List<MemberDTO> findAll() {
+        return sql.selectList("Member.findAll");
+    }
+
+    public MemberDTO findById(Long id) {
+        return sql.selectOne("Member.findById", id);
+    }
+
+    public void delete(Long id) {
+        sql.delete("Member.delete", id);
+    }
+
+    public MemberDTO findByMemberEmail(String loginEmail) {
+
+     return sql.selectOne("Member.findByMemberEmail", loginEmail);
+    }
+
+    public int update(MemberDTO memberDTO) {
+        return sql.update("Member.update", memberDTO);
     }
 }
